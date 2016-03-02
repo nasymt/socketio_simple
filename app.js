782ea711c19d1de335fe1f211acac37377c11df3
mtok <TadashiMatsuoka@MacBook-Pro.local>
2016-03-02 11:24:37 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var app = require('http').createServer(handler),
+	io = require('socket.io').listen(app),
+	fs = require('fs');
+	
+app.listen(1337);
+function handler(req,res){
+	fs.readFile('./index.html',function(err,data){
+		if(err){
+			res.writeHead(500);
+			return res.end('Error');
+		}
+		res.writeHead(200);
+		res.write(data);
+		res.end();
+	});
+}
+io.sockets.on('connection', function(socket){
+	console.log("okokok");
+	socket.on('emit_from_client', function(data){
+		console.log("receive");
+		socket.emit('emit_from_server' , data);
+	});
+});
